99024f6f770c0729179008110de7825d52da2d72
sajad1993gh <sajad1993gh@gmail.com>
2019-07-01 19:06:54 -0400

    added customer side and schema, working correctly

---
+++ bamazonCustomer.js
@@ -0,0 +1,115 @@
+var inquirer = require('inquirer');
+var mysql = require('mysql');
+var table = require('console.table');
+var values = [];
+require("dotenv").config();
+// console.log(process.env.HOST_KEY);
+
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.HOST_KEY,
+    database: "bamazon_db"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    purchase();
+});
+
+function continueShop() {
+    inquirer.prompt([{
+        type: "list",
+        name: "repeat",
+        message: "Will you like to continue shopping?",
+        choices: ["YES", "NO"]
+    }]).then(function(response) {
+        if (response.repeat === "NO") {
+            console.log("Thank you for shopping with Lawrence Fashion.Inc! See you next time.")
+            connection.end()
+        } else {
+            purchase()
+        }
+    });
+};
+
+function purchase() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        console.log(res);
+        values = [];
+        for (var i = 0; i < res.length; i++) {
+            values.push([res[i].item_id, res[i].product_name, res[i].department_name, "$" + res[i].price])
+        };
+        // console.log(values);
+        console.table(['Item ID', 'Item', 'Department', 'Price'], values);
+        inquirer.prompt([{
+                type: "input",
+                name: "product",
+                message: "What will you like to buy (enter Item ID)?",
+                validate: function(value) {
+                    if (!isNaN(value)) {
+                        return true
+                    } else {
+                        return false
+                    }
+                }
+            },
+            {
+                type: "input",
+                name: "quantity",
+                message: "How many will you like to buy?",
+                validate: function(value) {
+                    if (!isNaN(value)) {
+                        return true
+                    } else {
+                        return false
+                    }
+                }
+            }
+        ]).then(function(response) {
+            connection.query("SELECT * FROM products WHERE item_id=?", [response.product], function(err, res) {
+                if (err) throw err;
+                if (response.quantity > res[0].stock_quantity) {
+                    console.log("Sorry! We only have " + res[0].stock_quantity + " available for sale. Please reenter the quantity.")
+                    purchase();
+                } else {
+                    var sales = res[0].price * response.quantity;
+                    console.log("Your total is: $" + sales);
+                    var newSales = res[0].product_sales + sales;
+                    var newQuantity = res[0].stock_quantity - response.quantity;
+                    connection.query(
+                        "UPDATE products SET ? WHERE ?", [{
+                                stock_quantity: newQuantity
+                            },
+                            {
+                                item_id: response.product
+                            }
+                        ],
+                        function(err, res) {
+                            if (err) throw err;
+                            console.log(res.affectedRows + " inventory updates")
+                        }
+                    );
+                    connection.query(
+                        "UPDATE products SET ? WHERE ?", [{
+                                product_sales: newSales
+                            },
+                            {
+                                item_id: response.product
+                            }
+                        ],
+                        function(err, res) {
+                            if (err) throw err;
+                            console.log("Thank you for your purchase!")
+                            continueShop()
+                        }
+                    );
+                };
+            });
+        });
+    });
+};
---
+++ schema.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS bamazon_db;
+CREATE database bamazon_db;
+
+USE bamazon_db;
+
+CREATE TABLE products (
+    item_id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(100) NOT NULL, 
+	department_name VARCHAR(100) NOT NULL, 
+	price DECIMAL(11, 2) NOT NULL, 
+	stock_quantity INTEGER NOT NULL, 
+    PRIMARY KEY (item_id)
+);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+    VALUES("Rolex", "Watch", 10000, 50),
+        ("Printed Summer Tee", "Tops", 120, 100), 
+        ("Matte Black Blazer", "Tops", 300, 50), 
+        ("Distressed Jeans", "Bottoms", 250, 100), 
+        ("Classic Tanks", "Tops", 40, 500), 
+        ("Panerai", "Watch", 20000, 30), 
+        ("Printed Shorts", "Bottoms", 80, 100), 
+        ("Button Down Shirt", "Tops", 140, 60), 
+        ("Classic Trousers", "Bottoms", 180, 200),      
+        ("Frat Tanks", "Tops", 60, 200); 
+        
+
+ALTER TABLE products
+  ADD product_sales INTEGER NOT NULL DEFAULT 0;
+
+SELECT * FROM products;
